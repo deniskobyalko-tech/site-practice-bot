320bf8e19ed76b41c723f3866233c1b6b8119214
WORKSHOP <workshop@users.noreply.github.com>
2026-04-03 16:34:05 +0300

    fix: set API_BASE to /site-practice for nginx proxy

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -2,7 +2,7 @@ const tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 
-const API_BASE = "";
+const API_BASE = "/site-practice";
 const AUTH_HEADER = { Authorization: "tma " + tg.initData };
 
 const CHECKLIST_ITEMS = [
